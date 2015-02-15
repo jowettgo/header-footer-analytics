0a7c5d370b3266742501405630827b9d944c8d9f
Jowett Isaiah Go <jowettgo@gmail.com>
2015-02-15 10:51:24 -0800

    Update hfa.php
    add menu page html

--- hfa.php
+++ hfa.php
@@ -1,1 +1,31 @@
 /* main plugin file that will enqeue on theme's functions.php*/
+
+
+
+
+
+
+
+
+
+
+
+/* dashboard menu page */
+
+<html>
+
+<form method = "GET" action = "hfa_addCode.php">
+
+Paste your Google Webmaster Code here: <input type="text" name="gwebmaster">
+<br/>
+
+Paste your Google Analytics Code here: <input type="text" name="ganalytics">
+<br/>
+
+<input type="submit" value="Save">
+
+</form>
+
+
+
+</html>
